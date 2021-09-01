0833c6ad9272df44e17b82f3bc254762205e6929
normyp <pm.normington@gmail.com>
2021-09-01 12:02:33 +0100

    Print issues

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from github import Github
 import os
+import time
 
 # First create a Github instance:
 access_token = os.environ['ACCESS_TOKEN']
@@ -10,5 +11,6 @@ g = Github(access_token)
 g = Github(base_url="https://api.github.com", login_or_token=access_token)
 
 # Then play with your Github objects:
-for repo in g.get_user().get_repos():
-    print(repo.name)
+for issue in g.search_issues('', sort="created", order="desc", label="good-first-issue"):
+    print(issue)
+    time.sleep(2.0)
