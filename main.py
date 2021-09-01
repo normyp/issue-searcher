1284f5c90b32552a64627ae8b86d1eaad0779ae2
normyp <pm.normington@gmail.com>
2021-09-01 13:57:06 +0100

    Per page 100

--- main.py
+++ main.py
@@ -5,12 +5,14 @@ import time
 # First create a Github instance:
 access_token = os.environ['ACCESS_TOKEN']
 # using an access token
-g = Github(access_token)
 
 # Github Enterprise with custom hostname
-g = Github(base_url="https://api.github.com", login_or_token=access_token)
+g = Github(base_url="https://api.github.com", login_or_token=access_token, per_page=100)
 
+i = 0
 # Then play with your Github objects:
 for issue in g.search_issues('', sort="created", order="desc", label="good-first-issue"):
     print(issue)
-    time.sleep(2.0)
+    i += 1
+    print(i)
+
